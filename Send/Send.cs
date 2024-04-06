be6bbed1a9340da0ecab015d797291f1b40be0eb
ruhtar <arthur.amorim10@gmail.com>
2024-04-05 21:49:12 -0300

    Send message

--- Send/Send.cs
+++ Send/Send.cs
@@ -1,10 +1,32 @@
-﻿namespace Send
+﻿using RabbitMQ.Client;
+using System.Text;
+
+//Creating the connectiong to the server
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel(); //Connecting to a service is slow. As time goes on, connection warms up and connections get faster. So to bypass this, RabbitMQ creates the connection and inside this connection, Channels are created. They work similar to Connection Pools of Databases and allow 
+
+//To send, we must declare a queue for us to send to; then we can publish a message to the queue:
+
+channel.QueueDeclare(queue: "my-queue-name",
+                    durable: true,
+                    autoDelete: true,
+                    exclusive: false,
+                    arguments: null
+                    );
+while (true)
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+    Console.WriteLine("Send a message... \n");
+    var message = Console.ReadLine();
+    if (string.IsNullOrWhiteSpace(message)) break;
+
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(exchange: string.Empty,
+                         routingKey: "my-queue-name",
+                         basicProperties: null,
+                         body: body);
+    Console.WriteLine($" [X] Sent: {message} \n");
+
+    Console.WriteLine(" Press [enter] to exit. \n");
 }
