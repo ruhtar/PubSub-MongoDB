b21a00e7a0d9aa39b8759f1fc2460ceb8d39e788
ruhtar <arthur.amorim10@gmail.com>
2024-04-19 20:54:44 -0300

    Pix mock

---
+++ Receive/Pix.cs
@@ -0,0 +1,34 @@
+﻿namespace Receive;
+public class Pagador
+{
+    public string Nome { get; set; }
+    public string Banco { get; set; }
+    public string Conta { get; set; }
+    public string Agencia { get; set; }
+    public string CpfCnpj { get; set; }
+    public string TipoConta { get; set; }
+    public string TipoPessoa { get; set; }
+}
+
+public class ComponenteValor
+{
+    public string Original { get; set; }
+}
+
+public class Pix
+{
+    public string TxId { get; set; }
+    public string Chave { get; set; }
+    public decimal Valor { get; set; }
+    public DateTime Horario { get; set; }
+    public Pagador Pagador { get; set; }
+    public List<string> Devolucoes { get; set; }
+    public string EndToEndId { get; set; }
+    public string InfoPagador { get; set; }
+    public ComponenteValor ComponentesValor { get; set; }
+}
+
+public class PixCollection
+{
+    public List<Pix> Pix { get; set; }
+}
--- Send/Send.cs
+++ Send/Send.cs
@@ -11,23 +11,28 @@ var queueName = "my-queue-name";
 
 channel.QueueDeclare(queue: queueName,
                     durable: true,
-                    autoDelete: true,
+                    autoDelete: false,
                     exclusive: false,
                     arguments: null
                     );
-while (true)
+
+var message = "{\r\n    \"pix\": [\r\n        {\r\n            \"txId\": \"08f714646220ea4d52ba7d3c5d05361c3a\",\r\n            \"chave\": \"b2wa110c-eb6e-41b8-8c4d-ac4f354b03ea\",\r\n            \"valor\": \"19.37\",\r\n            \"horario\": \"2024-01-26T19:13:13.157Z\",\r\n            \"pagador\": {\r\n                \"nome\": \"xxxxx.\",\r\n                \"banco\": \"xxxxx\",\r\n                \"conta\": \"xxxx\",\r\n                \"agencia\": \"1\",\r\n                \"cpfCnpj\": \"xxxxx\",\r\n                \"tipoConta\": \"xxxx\",\r\n                \"tipoPessoa\": \"xxxxxx\"\r\n            },\r\n            \"devolucoes\": [],\r\n            \"endToEndId\": \"E306355055520240126191300352240410\",\r\n            \"infoPagador\": \"Serviço realizado.\",\r\n            \"componentesValor\": {\r\n                \"original\": \"19.37\"\r\n            }\r\n        }\r\n    ]\r\n}";
+Console.WriteLine("How many times do you want to send the message?");
+
+var times = Console.ReadLine();
+//if (string.IsNullOrWhiteSpace(message)) break;
+
+var body = Encoding.UTF8.GetBytes(message);
+
+for (int i = 0; i < Int32.Parse(times); i++)
 {
-    Console.WriteLine("Send a message... \n");
-    var message = Console.ReadLine();
-    if (string.IsNullOrWhiteSpace(message)) break;
-
-    var body = Encoding.UTF8.GetBytes(message);
-
     channel.BasicPublish(exchange: string.Empty,
-                         routingKey: queueName,
-                         basicProperties: null,
-                         body: body);
-    Console.WriteLine($" [X] Sent: {message} \n");
-
-    Console.WriteLine(" Press [enter] to exit. \n");
+                 routingKey: queueName,
+                 basicProperties: null,
+                 body: body);
 }
+
+Console.WriteLine($" [X] Sent: {message} \n");
+
+Console.WriteLine(" Press [enter] to exit. \n");
+Console.ReadLine();
